d6681665f5bdd13b3c92830ba83a5c7adf424d42
arafat25 <auddullah1@student.gsu.edu>
2017-04-18 23:59:38 -0400

    order confirm pages

---
+++ OrderController.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class OrderController {
+	
+	@FXML
+	private Button Cancel;
+	
+	@FXML
+	private Button Buy;
+
+	@FXML
+	private Label firstName;
+	
+	@FXML
+	private Label lastName;
+	
+	@FXML
+	private Label cardNum;
+	
+	@FXML
+	private Label expDate;
+
+	@FXML
+	private TextField firstNameTxt;
+
+	@FXML
+	private TextField lastNameTxt;
+	
+	@FXML
+	private TextField cardNumText;
+	
+	@FXML
+	private TextField expDateTxt;
+	
+	
+
+}
---
+++ orderConfirmController.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.awt.Label;
+
+import javafx.fxml.FXML;
+
+
+public class orderConfirmController {
+	
+	@FXML
+	private Label firstName;
+	
+	@FXML
+	private Label lastName;
+	
+	@FXML
+	private Label orderID;
+	
+	@FXML
+	private Label productName;
+	
+	@FXML
+	private Label creditcardNum;
+	
+	
+
+}
